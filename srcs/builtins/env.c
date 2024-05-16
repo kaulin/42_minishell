82788294d5b4723e384a68a8c93c052b13b7b99b
kkauhane <kkauhane@student.hive.fi>
2024-05-16 14:26:11 +0300

    Changed the iterating method

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 14:13:38 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/05/13 14:32:41 by kkauhane         ###   ########.fr       */
+/*   Updated: 2024/05/16 14:23:52 by kkauhane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,15 @@ Env without any argument is used to print out a list of all environment variable
 
 int env_builtin(t_data *data)
 {
+    int i;
+
+    i = 0;
     if (!data->envp)
         return (EXIT_FAILURE);//Should this give an error message?
-    while (*data->envp)
+    while (data->envp[i])
     {
-        printf("%s\n", *data->envp);
-        data->envp++;
+        printf("%s\n", data->envp[i]);
+        i++;
     }
     return (EXIT_SUCCESS);
 }
