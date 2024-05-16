2551865ce475801bcfc1ac9b3efb29088cc62552
Jaakko Junttila <jajuntti@student.hive.fi>
2024-05-16 10:31:33 +0300

    Makes a subdir for parser and moves src files

--- srcs/parser/parser_utils.c
+++ srcs/parser/parser_utils.c
@@ -1,20 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.c                                           :+:      :+:    :+:   */
+/*   parser_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/14 09:14:44 by jajuntti          #+#    #+#             */
-/*   Updated: 2024/05/14 10:34:40 by jajuntti         ###   ########.fr       */
+/*   Created: 2024/05/16 10:27:52 by jajuntti          #+#    #+#             */
+/*   Updated: 2024/05/16 10:28:04 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-
-int	parse(char *input, t_data *data)
-{
-	(void)input;
-	(void)data;
-	return (0);
-}
+#include "parser.h"
