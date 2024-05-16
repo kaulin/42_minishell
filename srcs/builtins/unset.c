05e5a7f125ed0419808d1172aa3cac2b20324f5d
kkauhane <kkauhane@student.hive.fi>
2024-05-16 14:31:32 +0300

    added functions unset_builtin and unset

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 14:13:22 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/05/16 14:05:39 by kkauhane         ###   ########.fr       */
+/*   Updated: 2024/05/16 14:28:59 by kkauhane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,67 @@
 /*
 ◦ unset with no options
 
-unset [name]
-Remove each variable or function name.
-If no options are supplied, each name refers to a variable; if there is no variable by that name, a function with that name, if any, is unset(?).
-Readonly variables and functions may not be unset. Some shell variables lose their special behavior if they are unset; such behavior is noted in the description of the individual variables. The return status is zero unless a name is readonly or may not be unset.
+Unset name...
+Remove each variable or function name. 
+EXIT STATUS        
+    0    All name operands were successfully unset.
+
+    >0    At least one name could not be unset.
+Each name refers to a variable; if there is no variable by that name, a function with that name, if any, is unset(?).
+Readonly variables and functions may not be unset.
+The memory for envp is managed by the operating system, and reallocation/deallocation is not required.
 */
 
-void    unset_builtin(t_data *data)
+int unset(t_data *data, char *variable)
 {
-    while (*data->env)//find the variable to unset and unset it if not found return error message?
-    {
+    size_t  len;
+    char    **pointer;
+    int     i;
 
+    i = 0;
+    len = ft_strlen(variable);
+    pointer = data->envp;
+    while (*data->envp)
+    {
+        if (ft_strncmp(*data->envp, variable, len) == 0 && (*data->envp)[len] == '=')
+        {
+            data->envp++;
+            continue ;
+        }
+        pointer[i] = *data->envp;
+        i++;
+        data->envp++;
     }
+    pointer[i] = NULL;
+    data->envp = pointer;
+    return (EXIT_SUCCESS);
+}
+
+int unset_builtin(t_data *data, char **cmds)
+{
+    int i;
+    int flag;
+
+    i = 0;
+    flag = 0;//this flags unsuccessfull unsets
+    cmds++;
+    if (!*cmds)
+        error_message(--cmds, "not enough arguments");
+    while (*cmds != '\0')
+    {
+        while (data->envp[i] && ft_strncmp(*cmds, data->envp[i], ft_strlen(*cmds) != 0))//find the variable to unset and unset it if not found return error message?
+            i++;
+        if (!data->envp[i])
+            flag = 1;
+        else
+            unset(data, *cmds);
+        i = 0;
+        cmds++;
+    }
+    if (flag != 0)
+    {
+        error_message(--cmds, "could not unset variable\n");//to be removed
+        return (EXIT_FAILURE);
+    }
+    return (EXIT_SUCCESS);
 }
