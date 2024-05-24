cfbe768ed01f1b619eb7a73a844166737700cd7e
Jaakko Junttila <jajuntti@student.hive.fi>
2024-05-24 10:51:53 +0300

    Adds functions for token list

---
+++ srcs/parser/token_list.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token_list.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 10:29:36 by jajuntti          #+#    #+#             */
+/*   Updated: 2024/05/24 10:48:05 by jajuntti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+/*
+Creates a new token struct and initialises its str with the parameter sting. 
+A pointer to the new_token is returned, unless there is a malloc 
+*/
+t_token	*token_new(char *content)
+{
+	t_token	*new_token;
+
+	new_token = malloc(sizeof(t_token));
+	if (!new_token)
+		return (NULL);
+	new_token->str = content;
+	define_token(new_token);
+	new_token->next = NULL;
+	return (new_token);
+}
+
+/*
+Adds the new_token to the end of the token_list. If token_list is empty to 
+start with, token_list is set equal to the new_token.
+*/
+void	token_add_back(t_token **token_list, t_token *new_token)
+{
+	t_token	*temp;
+
+	if (*token_list == NULL)
+		*token_list = new_token;
+	else
+	{
+		temp = *token_list;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new_token;
+	}
+}
+
+/*
+Frees the contents of the given token struct and then frees the struct pointer.
+*/
+void	token_delone(t_token *token)
+{
+	if (token == NULL)
+		return ;
+	if (token->str)
+		free(token->str);
+	free(token);
+}
+
+/*
+Frees all the tokens and their contents in a token_list.
+*/
+void	token_clear(t_token **token_list)
+{
+	t_token	*next;
+
+	if (token_list == NULL)
+		return ;
+	while (*token_list)
+	{
+		next = (*token_list)->next;
+		token_delone(*token_list);
+		*token_list = next;
+	}
+}
