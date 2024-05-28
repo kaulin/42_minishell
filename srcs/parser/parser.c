ee7db79558a74839d41c6c9ae1715af814f9243e
Jaakko Junttila <jajuntti@student.hive.fi>
2024-05-28 15:39:43 +0300

    Begins to add token parsing and variable expansion

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 09:14:44 by jajuntti          #+#    #+#             */
-/*   Updated: 2024/05/16 10:28:14 by jajuntti         ###   ########.fr       */
+/*   Updated: 2024/05/28 15:29:59 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,36 @@
 
 int	parse(char *input, t_data *data)
 {
-	(void)input;
-	(void)data;
+	t_parser parser;
+
+	init_parser(&parser);
+	while (*input)
+	{
+		skip_whitespace(&input);
+		if (!parser.quote_start && is_quote_char(*input))
+			parser.quote_start = input;
+		else if (parser.quote_start && *input == *parser.quote_start)
+		{
+			parser.substring = ft_substr(parser.quote_start, 1, input - parser.quote_start);
+			parser.quote_start = NULL;
+			if (!parser.substring)
+				return (1); // clean return
+			if (parser.quote_start == '\"')
+				expand_variables(&parser.substring, data);
+			parser.new_token = tokenize(parser.substring, *(input + 1));
+			token_add_back(parser.token_list, parser.new_token);
+
+
+		}
+	}
+	if (merge_tokens(parser.token_list))
+		return (1); // clean return
 	return (0);
 }
+
+/*
+Cases
+"$ec""$ho"
+"$ec""$space""$ho"
+"$ec""$null""$ho"
+*/
--- srcs/parser/parser_utils.c
+++ srcs/parser/parser_utils.c
@@ -6,8 +6,58 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 10:27:52 by jajuntti          #+#    #+#             */
-/*   Updated: 2024/05/16 10:28:04 by jajuntti         ###   ########.fr       */
+/*   Updated: 2024/05/28 15:36:08 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+
+/*
+CLEANER FUNCTION NEEDED
+*/
+void	init_parser(t_parser *parser)
+{
+	t_parser parser;
+	parser->substring = NULL;
+	parser->quote_start = NULL;
+	parser->string_start = NULL;
+	parser->token_list = NULL;
+	parser->new_token = NULL;
+}
+
+int	is_quote_char(char c)
+{
+	if (ft_strchr ("\'\"", c))
+		return (1);
+	return (0);
+}
+
+int	is_whitespace(char c)
+{
+	if (ft_strchr (" \t\v\n\r\f", c))
+		return (1);
+	return (0);
+}
+
+void	skip_whitespace(char **ptr)
+{
+	while (is_whitespace(**ptr))
+		(*ptr)++;
+}
+
+/*
+Expands the environment variables in the given string. Returns the expanded 
+string, or NULL if errors occur.
+*/
+char	*expand_variables(char **str, t_data *data)
+{
+/*
+	char	*original;
+	char	*tail;
+	char	*variable;
+	int		index;
+
+	original = *str;
+*/
+	return (NULL);
+}
