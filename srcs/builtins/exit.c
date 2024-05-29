c0f06a9cf01c5d3ff02ba2c580f0b03dd59bbd1f
kkauhane <kkauhane@student.hive.fi>
2024-05-29 15:14:18 +0300

    modified comments

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 14:13:52 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/05/13 14:28:49 by kkauhane         ###   ########.fr       */
+/*   Updated: 2024/05/29 14:40:24 by kkauhane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@
 ◦ exit with no options
 
 exit [n]
-Exit the shell, returning a status of n to the shell’s parent.
-If n is omitted, the exit status is that of the last command executed.
+Exit the shell, returning the status of the last command executed to the shell’s parent.
 Any trap on EXIT is executed before the shell terminates.
 */
+
+// int	exit_builtin()
+// {
+		//free everything
+		//return exitstatus
+// }
