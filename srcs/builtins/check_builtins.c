e408b3c2b3c22bbf3639cecb16b5bf6261e2cf0c
kkauhane <kkauhane@student.hive.fi>
2024-05-29 15:09:07 +0300

    modified pwd_builtins arguments

--- srcs/builtins/check_builtins.c
+++ srcs/builtins/check_builtins.c
@@ -6,7 +6,7 @@
 /*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 10:12:13 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/05/29 14:13:45 by kkauhane         ###   ########.fr       */
+/*   Updated: 2024/05/29 14:22:19 by kkauhane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,19 @@ This checks if the command is one of the builtins
 int check_if_builtin(t_data *data, char **cmds)
 {
 	if (ft_strncmp("echo", cmds[0], 5) == 0)
-	echo_builtin(cmds);
+		echo_builtin(cmds);
 	if (ft_strncmp("cd", cmds[0], 3) == 0)
-	cd_builtin(data, cmds);
+		cd_builtin(data, cmds);
 	if (ft_strncmp("env", cmds[0], 4) == 0)
-	env_builtin(data, cmds);
+		env_builtin(data, cmds);
 	// if (ft_strncmp("exit", cmds[0], 5) == 0)
-	// 	exit_builtin();
+		// 	exit_builtin();
 	if (ft_strncmp("export", cmds[0], 7) == 0)
-	export_builtin(data,cmds);
+		export_builtin(data,cmds);
 	if (ft_strncmp("pwd", cmds[0], 4) == 0)
-	pwd_builtin();
+		pwd_builtin(cmds);
 	if (ft_strncmp("unset", cmds[0], 6) == 0)
 		unset_builtin(data, cmds);
 	return (0);
 }
+//removed arg from pwd
