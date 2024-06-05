79674b41e7d5522fd1cd374f0d2d12c0d591a831
Pik-kak <143032102+Pik-kak@users.noreply.github.com>
2024-06-05 16:12:17 +0300

    removed copy_envp function

--- srcs/data.c
+++ srcs/data.c
@@ -12,32 +12,6 @@
 
 #include "minishell.h"
 
-/*
-This copies the envp variables and counts their amount
-*/
-char	**copy_envp(t_data *data, char **envp)
-{
-	char	**copy;
-	int		i;
-
-	i = 0;
-	while (envp[i])
-		i++;
-	copy = (char **)malloc(sizeof(char *) * i);
-	if (!copy)
-		return (NULL);
-	i = 0;
-	while (envp[i])
-	{
-		copy[i] = (char *)malloc(ft_strlen(envp[i]) + 1);
-		copy[i] = envp[i];
-		i++;
-	}
-	envp = copy;
-	data->envp_count = i;
-	return (copy);
-}
-
 void	clean_data(t_data *data)
 {
 	if (data->cmd_list)
