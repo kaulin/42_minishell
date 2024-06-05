de4404000d8211a0d7dba01e0ee2b65425419aa1
kkauhane <kkauhane@student.hive.fi>
2024-06-05 19:28:42 +0300

    shortened variable name

--- srcs/execute.c
+++ srcs/execute.c
@@ -3,36 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pik-kak <pik-kak@student.42.fr>            +#+  +:+       +#+        */
+/*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 16:49:16 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/06/05 16:09:02 by pik-kak          ###   ########.fr       */
+/*   Updated: 2024/06/05 19:28:33 by kkauhane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
-static void	child(int *fd, t_data *data, t_cmd *current_cmd)
+static void	child(int *fd, t_data *data, t_cmd *cur_cmd)
 {
+
 
 }
 
-static void	parent(t_data *data, t_cmd *current_cmd)
+static void	parent(t_data *data, t_cmd *cur_cmd)
 {
 	int		fd[2];
 
 	if (pipe(fd) == -1)
 		fail(666, "Pipe failed", data);
-	current_cmd->pid = fork();
-	if (current_cmd->pid == -1)
+	cur_cmd->pid = fork();
+	if (cur_cmd->pid == -1)
 	{
 		close(fd[0]);
 		close(fd[1]);
 		fail(666, "Fork failed", data);
 	}
-	if (current_cmd->pid == 0)
-		child(fd, data, current_cmd);
+	if (cur_cmd->pid == 0)
+		child(fd, data, cur_cmd);
 	close(fd[1]);
 	if (dup2(fd[0], STDIN_FILENO) == -1)
 	{
@@ -51,23 +51,23 @@ int	execute_and_pipe(t_data *data)
 	int		i;
 	int		exit_status;
 	int		status;
-	t_cmd	*current_cmd;
+	t_cmd	*cur_cmd;
 
 	i = 0;
-	current_cmd = data->cmd_list;
-	while (current_cmd != NULL)
+	cur_cmd = data->cmd_list;
+	while (cur_cmd != NULL)
 	{
-		parent(data, current_cmd);
-		current_cmd = current_cmd->next;
+		parent(data, cur_cmd);
+		cur_cmd = cur_cmd->next;
 	}
-	current_cmd = data->cmd_list;
-	while (current_cmd != NULL)
+	cur_cmd = data->cmd_list;
+	while (cur_cmd != NULL)
 	{
 		exit_status = 0;
-		if (waitpid(current_cmd->pid, status, 0) == -1)
+		if (waitpid(cur_cmd->pid, status, 0) == -1)
 			fail(1, "Waitpid failed", data);
 		if (WIFEXITED(status))
 			exit_status = WEXITSTATUS(status);
-		current_cmd = current_cmd->next;
+		cur_cmd = cur_cmd->next;
 	}
 }
