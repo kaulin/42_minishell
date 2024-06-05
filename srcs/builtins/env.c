460e9cdfe51f3584d2895560c23e0e95651944c2
pik-kak <karokauh@gmail.com>
2024-06-05 17:01:00 +0300

    Deleted extra copy_envp function

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: pik-kak <pik-kak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 14:13:38 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/05/29 14:38:46 by kkauhane         ###   ########.fr       */
+/*   Updated: 2024/06/05 17:00:19 by pik-kak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
+
+
 /*
 ◦ env with no options or arguments
 
@@ -52,44 +54,20 @@ int	arrange(char **copy, int i, int j)
 	return (EXIT_SUCCESS);
 }
 
-char	**copy_env(t_data *data, int len)
-{
-	int i;
-	char **copy;
-
-	i = 0;
-	copy = (char **)malloc((len + 1) * sizeof(char *));
-	if (!copy)
-		return (NULL);
-	while (data->envp[i])
-	{
-		copy[i] = (char *)malloc((ft_strlen(data->envp[i]) + 1) * sizeof(char));
-		if (!copy[i])
-			return (NULL);
-		copy[i] = data->envp[i];
-		i++;
-	}
-	copy[i] = NULL;
-	return (copy);
-}
-
 int	env_in_order(t_data *data)
 {
 	int i;
 	int j;
-	int len;
 	char **copy;
 
-	i = 0;len = 0;
-	while (data->envp[len])
-		len++;
-	copy = copy_env(data, len);
+	i = 0;
+	copy = copy_envp(data, data->envp);
 	if (!copy)
 		return (EXIT_FAILURE);
-	while (i < len - 1)
+	while (i < data->envp_count - 1)
 	{
 		j = i + 1;
-		while (j < len)
+		while (j < data->envp_count)
 		{
 			if (ft_strncmp(copy[i], copy[j], ft_strlen(copy[i]) + 1) > 0)
 				if (arrange(copy, i, j) == 1)
@@ -123,7 +101,7 @@ int env_builtin(t_data *data, char **cmds)
 		ft_putendl_fd("Error", STDERR);//error message?
 		return (EXIT_FAILURE);
 	}
-	while (data->envp[i])
+	while (data->envp[i] != NULL)
 	{
 		printf("%s\n", data->envp[i]);
 		i++;
