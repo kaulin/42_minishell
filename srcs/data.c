9ea35dba870a7612396795866fa89a4f16aed6ff
pik-kak <karokauh@gmail.com>
2024-06-05 16:48:15 +0300

    Added copy_envp function and modifier init_data

--- srcs/data.c
+++ srcs/data.c
@@ -3,16 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   data.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: pik-kak <pik-kak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 09:36:30 by jajuntti          #+#    #+#             */
-/*   Updated: 2024/06/05 12:01:46 by jajuntti         ###   ########.fr       */
+/*   Updated: 2024/06/05 16:47:27 by pik-kak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	clean_data(t_data *data)
+/*
+This copies the envp variables and counts their amount
+*/
+
+char	**copy_envp(t_data *data, char **envp)
+{
+	char	**copy;
+	int		i;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	copy = (char **)malloc(sizeof(char *) * (i + 1));
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (envp[i])
+	{
+		copy[i] = ft_strdup(envp[i]);
+		if (!copy[i])
+		{
+			while (i > 0)
+                free(copy[--i]);
+			free(copy);
+			return (NULL);
+		}
+		i++;
+	}
+	copy[i] = NULL;
+	data->envp_count = i;
+	return (copy);
+}
+
+void	clean_data(t_data *data)//need to add envp_clear
 {
 	if (data->cmd_list)
 		cmd_clear(&data->cmd_list);
@@ -23,6 +56,8 @@ void	clean_data(t_data *data)
 void	init_data(t_data *data, char **envp)
 {
 	data->envp = copy_envp(data, envp);
+	//if (data->envp == NULL)//if copy failed
+		//error();
 	data->cmd_list = NULL;
 	data->status = 0;
 	data->error_msg = NULL;
