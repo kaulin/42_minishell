01fd7b454a2bfac79c2c0f9009ee313a72b6cfee
Jaakko Junttila <jajuntti@student.hive.fi>
2024-06-05 12:02:49 +0300

    Moves copy_envp to data.c from check_builtins.c

--- srcs/builtins/check_builtins.c
+++ srcs/builtins/check_builtins.c
@@ -3,51 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   check_builtins.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 10:12:13 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/05/30 15:06:44 by kkauhane         ###   ########.fr       */
+/*   Updated: 2024/06/05 12:01:59 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
-
-
-/*
-This copies the envp variables and counts their amount
-*/
-char	**copy_envp(t_data *data, char **envp)
-{
-	char	**copy;
-	int		i;
-
-	i = 0;
-	while (envp[i])
-		i++;
-	copy = (char **)malloc(sizeof(char *) * i);
-	if (!copy)
-		return (NULL);
-	i = 0;
-	while (envp[i])
-	{
-		copy[i] = (char *)malloc(ft_strlen(envp[i]) + 1);
-		copy[i] = envp[i];
-		i++;
-	}
-	envp = copy;
-	data->envp_count = i;
-	return (copy);
-}
-
-void	init_data(t_data *data, char **envp)
-{
-	data->envp = copy_envp(data, envp);
-	// if (data->envp == NULL)
-	// 	error();
-	data->cmd_list = NULL;
-	data->status = 0;
-	data->error_msg = NULL;
-}
 
 /*
 This checks if the command is one of the builtins
--- srcs/data.c
+++ srcs/data.c
@@ -6,11 +6,37 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 09:36:30 by jajuntti          #+#    #+#             */
-/*   Updated: 2024/05/23 12:31:22 by jajuntti         ###   ########.fr       */
+/*   Updated: 2024/06/05 12:01:46 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/*
+This copies the envp variables and counts their amount
+*/
+char	**copy_envp(t_data *data, char **envp)
+{
+	char	**copy;
+	int		i;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	copy = (char **)malloc(sizeof(char *) * i);
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (envp[i])
+	{
+		copy[i] = (char *)malloc(ft_strlen(envp[i]) + 1);
+		copy[i] = envp[i];
+		i++;
+	}
+	envp = copy;
+	data->envp_count = i;
+	return (copy);
+}
 
 void	clean_data(t_data *data)
 {
@@ -22,7 +48,7 @@ void	clean_data(t_data *data)
 
 void	init_data(t_data *data, char **envp)
 {
-	data->envp = envp;
+	data->envp = copy_envp(data, envp);
 	data->cmd_list = NULL;
 	data->status = 0;
 	data->error_msg = NULL;
