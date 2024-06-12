96990029b3a2e525cc3ea47c01e28004595a3dfd
kkauhane <kkauhane@student.hive.fi>
2024-06-12 11:46:52 +0300

    Added check_if_builtin call

--- srcs/execute.c
+++ srcs/execute.c
@@ -6,7 +6,7 @@
 /*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 16:49:16 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/06/10 14:45:27 by kkauhane         ###   ########.fr       */
+/*   Updated: 2024/06/12 11:38:25 by kkauhane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,10 @@ int	do_cmd(t_data *data, t_cmd *cur_cmd)
 
 /*
 Scenarios:
+- Only command
+	- We have eg. echo/pwd testing > test.txt or cat << LIMIT
 - First command:
-	- We have heredoc but no infile//we reed from terminal
+	- We have heredoc but no infile//we read from terminal
 	- We have infile (redirection), we read from infile
 	- We have heredoc and infile. This might be a syntax error and we do not need to handle it?
 	- We don't have redirection
@@ -46,31 +48,31 @@ Scenarios:
 	- We have heredoc but no outfile
 	- We have heredoc and outfile
 */
-// static void	open_dub_close(t_data *data, t_cmd *cur_cmd)
-// {
-// 	if (data->cmd_list == cur_cmd)//we check if the command is the very first
-// 	{
-// 		if (cur_cmd->heredoc_flag && !cur_cmd->infile) //heredoc flag but no infile do we need this?
-// 			cur_cmd->in_fd = STDIN_FILENO;
-// 		if (!cur_cmd->heredoc_flag && cur_cmd->infile)//redirection we open the infile to stdin.
-// 			cur_cmd->in_fd = open(cur_cmd->infile, O_RDONLY);
-// 		if (cur_cmd->in_fd == -1 || dup2(cur_cmd->in_fd, STDIN_FILENO) == -1)//if there was an error in opening the file or with replacing the fd 1 with the file fd
-// 			fail("bash: syntax error near unexpected token `newline'");
-// 		close(cur_cmd->in_fd);
-// 	}
-// 	if (cur_cmd->next == NULL)//if it is the last command
-// 	{
-// 		if (cur_cmd->append_flag && cur_cmd->outfile)//if there is append flag and outfile we append into the end of the outfile
-// 			cur_cmd->out_fd = open(cur_cmd->outfile, O_APPEND | O_CREAT | O_RDWR, 0644);
-// 		if (cur_cmd->append_flag && !cur_cmd->outfile)//if there is heredoc but no outfile we have error, do we need this?
-// 			fail();
-// 		else
-// 			cur_cmd->out_fd = open(cur_cmd->outfile, O_TRUNC | O_CREAT | O_RDWR, 0644);//if there is no heredoc but outfile
-// 		if (cur_cmd->out_fd == -1 || dup2(cur_cmd->out_fd, STDOUT_FILENO) == -1)//if there was an error in opening the file or with replacing the fd 0 with the file fd
-// 			fail();
-// 		close(cur_cmd->out_fd);
-// 	}
-// }
+static void	open_dub_close(t_data *data, t_cmd *cur_cmd)
+{
+	if (data->cmd_list == cur_cmd)//we check if the command is the very first
+	{
+		if (cur_cmd->heredoc_flag && !cur_cmd->infile) //heredoc flag but no infile do we need this?
+			cur_cmd->in_fd = STDIN_FILENO;
+		if (!cur_cmd->heredoc_flag && cur_cmd->infile)//redirection we open the infile to stdin.
+			cur_cmd->in_fd = open(cur_cmd->infile, O_RDONLY);
+		if (cur_cmd->in_fd == -1 || dup2(cur_cmd->in_fd, STDIN_FILENO) == -1)//if there was an error in opening the file or with replacing the fd 1 with the file fd
+			fail("bash: syntax error near unexpected token `newline'");
+		close(cur_cmd->in_fd);
+	}
+	if (cur_cmd->next == NULL)//if it is the last command
+	{
+		if (cur_cmd->append_flag && cur_cmd->outfile)//if there is append flag and outfile we append into the end of the outfile
+			cur_cmd->out_fd = open(cur_cmd->outfile, O_APPEND | O_CREAT | O_RDWR, 0644);
+		if (cur_cmd->append_flag && !cur_cmd->outfile)//if there is heredoc but no outfile we have error, do we need this?
+			fail();
+		else
+			cur_cmd->out_fd = open(cur_cmd->outfile, O_TRUNC | O_CREAT | O_RDWR, 0644);//if there is no heredoc but outfile
+		if (cur_cmd->out_fd == -1 || dup2(cur_cmd->out_fd, STDOUT_FILENO) == -1)//if there was an error in opening the file or with replacing the fd 0 with the file fd
+			fail();
+		close(cur_cmd->out_fd);
+	}
+}
 
 /*
 Child process handles input and output redirection and calls do_cmd to actually 
@@ -89,8 +91,8 @@ static void	child(int *fd, t_data *data, t_cmd *cur_cmd)
 		//fail(127, (*piper)->cmdv[(*piper)->cmd_i], piper);
 	open_dub_close(data, cur_cmd);
 	err_code = do_cmd(data, cur_cmd);
-	//if (err_code == 1)
-		//fail(err_code, "Memory allocation error", piper);
+	if (err_code == 1)
+		fail();
 	//if ((*piper)->cmd_err)
 		//fail(err_code, (*piper)->cmd_err, piper);
 	//fail(err_code, (*piper)->cmdv[(*piper)->cmd_i], piper);
@@ -102,15 +104,18 @@ static void	parent(t_data *data, t_cmd *cur_cmd)
 
 	if (pipe(fd) == -1)
 		fail(666, "Pipe failed", data);
-	cur_cmd->pid = fork();
-	if (cur_cmd->pid == -1)
+	if (check_if_builtin(data, cur_cmd->cmd_arr) == 1)//if the command is not a builtin
 	{
-		close(fd[0]);
-		close(fd[1]);
-		fail(666, "Fork failed", data);
+		cur_cmd->pid = fork();
+		if (cur_cmd->pid == -1)
+		{
+			close(fd[0]);
+			close(fd[1]);
+			fail(666, "Fork failed", data);
+		}
+		if (cur_cmd->pid == 0)
+			child(fd, data, cur_cmd);
 	}
-	if (cur_cmd->pid == 0)
-		child(fd, data, cur_cmd);
 	close(fd[1]);
 	if (dup2(fd[0], STDIN_FILENO) == -1)
 	{
