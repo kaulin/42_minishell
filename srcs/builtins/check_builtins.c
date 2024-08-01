c124705062587e54856b7f6b12fcb44a5938aac9
pik-kak <karokauh@gmail.com>
2024-08-01 13:40:36 +0300

    divided check_if_builtin() to two different functions

--- srcs/builtins/check_builtins.c
+++ srcs/builtins/check_builtins.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_builtins.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: pikkak <pikkak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 10:12:13 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/06/12 11:43:04 by kkauhane         ###   ########.fr       */
+/*   Updated: 2024/08/01 13:38:54 by pikkak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,26 @@
 
 /*
 This checks if the command is one of the builtins
+*/
+
+int	check_if_builtin(char **cmds)
+{
+	if (ft_strncmp("echo", cmds[0], 5) == 0
+		|| ft_strncmp("cd", cmds[0], 3) == 0
+			|| ft_strncmp("env", cmds[0], 4) == 0
+				|| ft_strncmp("exit", cmds[0], 5) == 0
+					|| ft_strncmp("export", cmds[0], 7) == 0
+						|| ft_strncmp("pwd", cmds[0], 4) == 0
+							|| ft_strncmp("unset", cmds[0], 6) == 0)
+		return (1);
+	return (0);
+}
+
+/*
+Executes the builtin in question
 Commands that produce output can be redirected so we we need to check redirections before executing commands.
 */
-int	check_if_builtin(t_data *data, char **cmds)//
+int	execute_builtin(t_data *data, char **cmds)
 {
 	(void)data;
 	if (ft_strncmp("echo", cmds[0], 5) == 0)
