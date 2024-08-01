78492e50e47425ccaa292e07aedb077b5917daca
pik-kak <karokauh@gmail.com>
2024-08-01 14:30:28 +0300

    added a comment and changed execute_builtin() to void

--- srcs/builtins/check_builtins.c
+++ srcs/builtins/check_builtins.c
@@ -6,7 +6,7 @@
 /*   By: pikkak <pikkak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 10:12:13 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/08/01 13:38:54 by pikkak           ###   ########.fr       */
+/*   Updated: 2024/08/01 14:20:39 by pikkak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,9 @@ int	check_if_builtin(char **cmds)
 /*
 Executes the builtin in question
 Commands that produce output can be redirected so we we need to check redirections before executing commands.
+How do we handle builtins failing?
 */
-int	execute_builtin(t_data *data, char **cmds)
+void	execute_builtin(t_data *data, char **cmds)
 {
 	(void)data;
 	if (ft_strncmp("echo", cmds[0], 5) == 0)
@@ -50,7 +51,4 @@ int	execute_builtin(t_data *data, char **cmds)
 		pwd_builtin(cmds);
 	if (ft_strncmp("unset", cmds[0], 6) == 0)
 		unset_builtin(data, cmds);
-	else
-		return (1);
-	return (0);
 }
