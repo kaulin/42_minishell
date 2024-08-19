1a8dc8bdb0e18f8a15d82017c8811ba20ae02eff
pik-kak <karokauh@gmail.com>
2024-08-19 10:05:34 +0300

    made some changes to child by dividing sentences

--- srcs/execute.c
+++ srcs/execute.c
@@ -6,7 +6,7 @@
 /*   By: pikkak <pikkak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 16:49:16 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/08/17 13:04:21 by pikkak           ###   ########.fr       */
+/*   Updated: 2024/08/17 20:37:42 by pikkak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,18 +40,24 @@ execute execve.
 static void	child(t_data *data, t_cmd *cur_cmd, int *fd)
 {
 	int	err_code;
-
-	if (cur_cmd == data->cmd_list && dup2(fd[0], STDIN_FILENO) == -1)// If this is not the first command, we need to read from the pipe
+	
+	if (cur_cmd != data->cmd_list)// If this is not the first command, we need to read from the pipe
 	{
-		close(fd[0]);
-		close(fd[1]);
-		fail(1, "Dup2 failed", data);
+		if (dup2(fd[0], STDIN_FILENO) == -1)
+		{
+			close(fd[0]);
+			close(fd[1]);
+			fail(1, "Dup2 failed", data);
+		}
 	}
-	if (cur_cmd->next != NULL && dup2(fd[1], STDOUT_FILENO) == -1)// If this is not the last command, we need to write to the pipe
+	if (cur_cmd->next != NULL)// If this is not the last command, we need to write to the pipe
 	{
-		close(fd[0]);
-		close(fd[1]);
-		fail(1, "Dup2 failed", data);
+		if (dup2(fd[1], STDOUT_FILENO) == -1)
+		{
+			close(fd[0]);
+			close(fd[1]);
+			fail(1, "Dup2 failed", data);
+		}
 	}
 	close(fd[0]);
 	close(fd[1]);
@@ -68,9 +74,9 @@ static void	child(t_data *data, t_cmd *cur_cmd, int *fd)
 
 static void	parent(t_data *data, t_cmd *cur_cmd)
 {
-	int		fd[2];
+	int	fd[2];
 
-	if (pipe(fd) == -1)
+	if (pipe(fd) == -1) 
 		fail(666, "Pipe failed", data);
 	cur_cmd->pid = fork();
 	if (cur_cmd->pid == -1)
@@ -92,8 +98,8 @@ static void	parent(t_data *data, t_cmd *cur_cmd)
 
 /*
 Goes through the linked list and calls the parent function for each node (cmd) of the list. Then waits for all the children to finish
-*/
 
+*/
 int	execute_and_pipe(t_data *data)
 {
 	int		exit_status;
@@ -113,7 +119,7 @@ int	execute_and_pipe(t_data *data)
 	}
 	else 
 	{	
-		while (cur_cmd != NULL)
+		while (cur_cmd != NULL)//we call the parent as many times a there are commands
 		{
 			cur_cmd->cmd_arr = ft_split(cur_cmd->cmd_str, " ");
 			parent(data, cur_cmd);
@@ -135,4 +141,34 @@ int	execute_and_pipe(t_data *data)
 	close(data->o_stdout);
 	return (exit_status);
 }
+
+/*
+static void	parent(t_data *data, t_cmd *cur_cmd)
+{
+	int		fd[2];
+
+	while (cur_cmd != NULL)
+	{
+		cur_cmd->cmd_arr = ft_split(cur_cmd->cmd_str, " ");
+		if (pipe(fd) == -1)//make the pipe
+			fail(666, "Pipe failed", data);
+		cur_cmd->pid = fork();//make the child
+		if (cur_cmd->pid == -1)
+		{
+			close(fd[0]);
+			close(fd[1]);
+			fail(666, "Fork failed", data);
+		}
+		if (cur_cmd->pid == 0)
+			child(data, cur_cmd, fd);
+		cur_cmd = cur_cmd->next;
+	}
+	close(fd[1]);//parent closes write end
+	if (dup2(fd[0], STDIN_FILENO) == -1)
+	{
+		close(fd[0]);
+		fail(666, "Dup2 failed", data);
+	}
+	close(fd[0]);
+}*/
 
