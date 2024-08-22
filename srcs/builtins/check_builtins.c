35f6ba30b2a4ffa9badc5f06993ff6438e9ea9ba
pik-kak <karokauh@gmail.com>
2024-08-22 13:47:58 +0300

    Corrected return types to ERROR and SUCCESS and removed extra tabs

--- srcs/builtins/check_builtins.c
+++ srcs/builtins/check_builtins.c
@@ -6,7 +6,7 @@
 /*   By: pikkak <pikkak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 10:12:13 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/08/01 14:20:39 by pikkak           ###   ########.fr       */
+/*   Updated: 2024/08/22 13:44:05 by pikkak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ int	check_if_builtin(char **cmds)
 {
 	if (ft_strncmp("echo", cmds[0], 5) == 0
 		|| ft_strncmp("cd", cmds[0], 3) == 0
-			|| ft_strncmp("env", cmds[0], 4) == 0
-				|| ft_strncmp("exit", cmds[0], 5) == 0
-					|| ft_strncmp("export", cmds[0], 7) == 0
-						|| ft_strncmp("pwd", cmds[0], 4) == 0
-							|| ft_strncmp("unset", cmds[0], 6) == 0)
-		return (1);
-	return (0);
+		|| ft_strncmp("env", cmds[0], 4) == 0
+		|| ft_strncmp("exit", cmds[0], 5) == 0
+		|| ft_strncmp("export", cmds[0], 7) == 0
+		|| ft_strncmp("pwd", cmds[0], 4) == 0
+		|| ft_strncmp("unset", cmds[0], 6) == 0)
+		return (ERROR);
+	return (SUCCESS);
 }
 
 /*
@@ -34,21 +34,22 @@ Executes the builtin in question
 Commands that produce output can be redirected so we we need to check redirections before executing commands.
 How do we handle builtins failing?
 */
-void	execute_builtin(t_data *data, char **cmds)
+int	execute_builtin(t_data *data, char **cmds)
 {
 	(void)data;
 	if (ft_strncmp("echo", cmds[0], 5) == 0)
-		echo_builtin(cmds);
+		return (echo_builtin(cmds));
 	if (ft_strncmp("cd", cmds[0], 3) == 0)
-		cd_builtin(data, cmds);
+		return (cd_builtin(data, cmds));
 	if (ft_strncmp("env", cmds[0], 4) == 0)
-		env_builtin(data, cmds);
+		return (env_builtin(data, cmds));
 	// if (ft_strncmp("exit", cmds[0], 5) == 0)
-		// 	exit_builtin();
+		//exit_builtin();
 	if (ft_strncmp("export", cmds[0], 7) == 0)
-		export_builtin(data, cmds);
+		return (export_builtin(data, cmds));
 	if (ft_strncmp("pwd", cmds[0], 4) == 0)
-		pwd_builtin(cmds);
+		return (pwd_builtin(cmds));
 	if (ft_strncmp("unset", cmds[0], 6) == 0)
-		unset_builtin(data, cmds);
+		return (unset_builtin(data, cmds));
+	return (SUCCESS);
 }
