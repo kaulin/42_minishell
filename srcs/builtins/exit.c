f4af825a607bef6cb434eadf9bf32f620e4cc95c
Jaakko Junttila <jajuntti@student.hive.fi>
2024-08-27 09:19:15 +0300

    Fixes return type

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pikkak <pikkak@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 14:13:52 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/08/26 20:29:13 by pikkak           ###   ########.fr       */
+/*   Updated: 2024/08/27 09:18:59 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,5 @@ Should this be void?
 int	exit_builtin(t_data *data)//do we need to have an exit_code variable in data to preserve the last exit code??
 {
 	fail(0, "Exit\n", data);
-	return ();
+	return (SUCCESS); // should this be a void function?
 }
