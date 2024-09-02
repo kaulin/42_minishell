e7ba0d9ad61f800e231ff5a1d8c254da722a1b8a
pik-kak <karokauh@gmail.com>
2024-09-02 22:36:41 +0300

    Added check_file() function

--- srcs/exec_utils.c
+++ srcs/exec_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: pikkak <pikkak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:00:35 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/08/26 13:02:18 by kkauhane         ###   ########.fr       */
+/*   Updated: 2024/09/02 22:28:05 by pikkak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,21 @@ void	reset_io(t_data *data)
 		data->error_msg = ft_strdup("Dup2 failed\n");
 	close(data->o_stdin);
 	close(data->o_stdout);
+}
+
+int	check_file(t_data *data, char *file, int flag)
+{
+	if (is_directory(file) == 1)
+		return (data->error_msg = ft_strdup("Is a directory\n"), ERROR);
+	if (flag == 1)//input
+	{
+		if (open(file, O_WRONLY | O_CREAT | O_TRUNC, 0666) == -1) 
+			return (data->error_msg = ft_strdup("Permissiggon denied\n"), ERROR);
+	}
+	if (flag == 2)//output
+	{
+		if (open(file, O_WRONLY | O_CREAT | O_TRUNC, 0666) == -1) 
+			return (data->error_msg = ft_strdup("Permission denied\n"), ERROR);
+	}
+	return (SUCCESS);
 }
