6375b6b141cb93fdba3e9eb426f87b2cd87de20d
pik-kak <karokauh@gmail.com>
2024-09-04 13:01:44 +0300

    Added a condition in case we have an append, fixed some norm errors

--- srcs/exec_utils.c
+++ srcs/exec_utils.c
@@ -6,7 +6,7 @@
 /*   By: pikkak <pikkak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 13:00:35 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/09/03 22:15:40 by pikkak           ###   ########.fr       */
+/*   Updated: 2024/09/04 12:59:27 by pikkak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ Resets the original in/out filedescriptors
 */
 void	reset_io(t_data *data)
 {
-	if (dup2(data->o_stdin, STDIN_FILENO) == -1 || dup2(data->o_stdout, STDOUT_FILENO) == -1)
+	if (dup2(data->o_stdin, STDIN_FILENO) == -1 || \
+		dup2(data->o_stdout, STDOUT_FILENO) == -1)
 		data->error_msg = ft_strdup("Dup2 failed\n");
 	close(data->o_stdin);
 	close(data->o_stdout);
@@ -26,24 +27,37 @@ void	reset_io(t_data *data)
 /*
 Checks if a file is a directory.
 If it is an infile, checks if it exists and has the right to read
-if it is an outfile, checks if it has the rights to write, if the file exists empties it and if not creates it.
+if it is an outfile, checks if it has the rights to write,
+if the file exists empties it and if not creates it.
 */
 
-int	check_file(t_data *data, char *file, int flag)
+int	check_file(t_data *data, t_file *cur_file, int flag)
 {
-	if (is_directory(file) == 1)
+	if (is_directory(cur_file->file_str) == 1)
 		return (data->error_msg = ft_strdup("Is a directory\n"), ERROR);
-	if (flag == 1)//input
+	if (flag == 1)
 	{
-		if (access(file, F_OK) == -1)
-			return (data->error_msg = ft_strdup("No such file or directory\n"), ERROR);
-		if (open(file, O_RDONLY) == -1) 
+		if (access(cur_file->file_str, F_OK) == -1)
+			return (data->error_msg = ft_strdup("No such file or directory\n"), \
+					ERROR);
+		if (open(cur_file->file_str, O_RDONLY) == -1)
 			return (data->error_msg = ft_strdup("Permission denied\n"), ERROR);
 	}
-	if (flag == 2)//output
+	if (flag == 2)
 	{
-		if (open(file, O_WRONLY | O_CREAT | O_TRUNC, 0666) == -1) 
-			return (data->error_msg = ft_strdup("Permission denied\n"), ERROR);
+		if (cur_file->flag)
+		{
+			if (open(cur_file->file_str, O_WRONLY | O_CREAT, 0666) == -1)
+				return (data->error_msg = ft_strdup("Permission denied\n"), \
+						ERROR);
+		}
+		else
+		{
+			if (open(cur_file->file_str, \
+				O_WRONLY | O_CREAT | O_TRUNC, 0666) == -1)
+				return (data->error_msg = ft_strdup("Permission denied\n"), \
+						ERROR);
+		}
 	}
 	return (SUCCESS);
 }
