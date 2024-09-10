46ad328d7bf5126ee6c5814e35f0b3c49ca38a44
kkauhane <karokauh@gmail.com>
2024-09-10 17:05:22 +0300

    completely rearranged signal.c

--- srcs/signal.c
+++ srcs/signal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pikkak <pikkak@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kkauhane <kkauhane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 19:43:22 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/09/06 17:32:01 by pikkak           ###   ########.fr       */
+/*   Updated: 2024/09/10 16:55:42 by kkauhane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,53 @@ Tcsetattr sets attributes of the terminal associated with STDIN,
 using the values stored in handler.
 */
 
-void	sigint_handler(int signal)
+void child_signal_handler(int sig)
 {
-	struct termios	handler;
-
-	(void)signal;
-	if (tcgetattr(0, &handler) == -1)
-		perror("Error in retrieving the terminal attributes\n");
-	if (tcsetattr(0, 0, &handler) == -1)
-		perror("Error in setting the terminal attributes\n");
-	write(1, "\n", 1);
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
+	if (sig == SIGINT)
+		exit(EXIT_SUCCESS);
 }
+
+void signal_handler(int sig)
+{
+	if (in_heredoc)
+	{
+		if (sig == SIGINT)
+		{
+			write(1, "\n", 1);
+			rl_on_new_line();
+			rl_replace_line("", 0);
+			//rl_redisplay();
+		}
+	}
+	else
+	{
+		if (sig == SIGINT)
+		{
+			write(1, "\n", 1);
+			rl_on_new_line();
+			rl_replace_line("", 0);
+			rl_redisplay(); 
+		}
+	}
+}
+
+void setup_signal_handling(void (*handler)(int))
+{
+	struct sigaction sa;
+
+	if (!handler)
+		sa.sa_handler = signal_handler;
+	else
+		sa.sa_handler = handler;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+
+	sigaction(SIGINT, &sa, NULL);
+	//if (sigaction(SIGINT, &sa, NULL) == -1) {
+	//	perror("sigaction");
+	//	exit(EXIT_FAILURE);
+	//}
+}
+
+
+
