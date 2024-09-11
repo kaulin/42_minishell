56d711c618698f7525cf9e8514010b99f404c395
Jaakko Junttila <jajuntti@student.hive.fi>
2024-09-11 13:31:53 +0300

    Makes pwd handle cases where getcwd fails and PWD is unset in env

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 14:12:34 by kkauhane          #+#    #+#             */
-/*   Updated: 2024/09/11 12:25:38 by jajuntti         ###   ########.fr       */
+/*   Updated: 2024/09/11 13:30:09 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,8 @@ int	pwd_builtin(t_data *data, char **cmds)
 		printf("%s\n", cwd);
 	else if (var_get_var(data->envp_list, "PWD"))
 		printf("%s\n", var_get_value(data->envp_list, "PWD"));
-	//else if (data->cwd && *data->cwd)
-	//	printf("%s\n", data->cwd);
+	else if (data->cwd && *data->cwd)
+		printf("%s\n", data->cwd);
 	else
 		return (oops(data, 1, NULL, "pwd failed"));
 	return (SUCCESS);
