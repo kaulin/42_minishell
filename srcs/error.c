3d1157c41b058ceee4b435d3091a009322f8bdd4
Jaakko Junttila <jajuntti@student.hive.fi>
2024-09-18 12:20:52 +0300

    Fixes norm errors in error.c

--- srcs/error.c
+++ srcs/error.c
@@ -6,21 +6,30 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 08:24:58 by jajuntti          #+#    #+#             */
-/*   Updated: 2024/09/11 15:29:28 by jajuntti         ###   ########.fr       */
+/*   Updated: 2024/09/18 12:20:08 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	syntax_error(char *e_str)
+static void	syntax_or_permission_error(int e_code, char *e_file, char *e_str)
 {
-	if (!e_str)
-		ft_putstr_fd("mini: syntax error - unclosed quotations\n", 2);
+	if (e_code == SYNTAX_E)
+	{
+		if (!e_str)
+			ft_putstr_fd("mini: syntax error - unclosed quotations\n", 2);
+		else
+		{
+			ft_putstr_fd("mini: syntax error - unexpected token '", 2);
+			ft_putstr_fd(e_str, 2);
+			ft_putendl_fd("'", 2);
+		}
+	}
 	else
 	{
-		ft_putstr_fd("mini: syntax error - unexpected token '", 2);
-		ft_putstr_fd(e_str, 2);
-		ft_putendl_fd("'", 2);
+		ft_putstr_fd("mini: ", 2);
+		ft_putstr_fd(e_file, 2);
+		ft_putendl_fd(": Permission denied", 2);
 	}
 }
 
@@ -35,13 +44,6 @@ static int	cmd_not_found_error(char *e_file)
 	ft_putstr_fd(e_file, 2);
 	ft_putendl_fd(": command not found", 2);
 	return (127);
-}
-
-static void	no_permissions_error(char *e_file)
-{
-	ft_putstr_fd("mini: ", 2);
-	ft_putstr_fd(e_file, 2);
-	ft_putendl_fd(": Permission denied", 2);
 }
 
 static int	cd_error(char *e_file)
@@ -63,12 +65,10 @@ int	oops(t_data *data, int e_code, char *e_file, char *e_str)
 		ft_putstr_fd(": ", 2);
 		ft_putendl_fd(e_str, 2);
 	}
-	else if (e_code == SYNTAX_E)
-		syntax_error(e_str);
+	else if (e_code == SYNTAX_E || (e_code == CANT_EXE_E && !e_str))
+		syntax_or_permission_error(e_code, e_file, e_str);
 	else if (e_code == NO_CMD_E)
 		e_code = cmd_not_found_error(e_file);
-	else if (e_code == CANT_EXE_E && !e_str)
-		no_permissions_error(e_file);
 	else if (e_code == CD_E)
 		e_code = cd_error(e_file);
 	else if (!e_file && e_str)
